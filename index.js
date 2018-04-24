497bbf749306ddc6b1e6b0c1ca38ac52550ca353
Duc Dang <mrtrinh5293@gmail.com>
2018-04-24 20:56:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-// write your code below!
-console.log("Hello, World!")
+if (false) {
+   console.log("This will never get printed because the above statement evaluates to false.")
+} else {
+   console.log("This will get printed!")
+}
